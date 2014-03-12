c49025ff77dbe7db932ed9a4f8323e87ba88f502
Andrew Cumming <andrew.cumming@gmail.com>
2014-03-11 23:56:00 -0400

    Updated mcee.py and mcplot.py

--- mcee.py
+++ mcee.py
@@ -4,7 +4,6 @@ import matplotlib
 matplotlib.use('macosx')
 import matplotlib.pyplot as plt
 import emcee
-import triangle
 import uuid
 import time
 import subprocess
@@ -12,10 +11,62 @@ import re
 import os
 import datetime
 
+
+def main():
+
+	nwalkers, ndim = 200, 6
+	dir = '12'
+	if os.path.exists('mcmc/'+dir):
+		print 'The output directory mcmc/'+dir+' already exists!'
+		exit()
+
+	# clean up temporary files if there are any left over from a previous run
+	os.system('rm /tmp/init.dat.*')
+
+	# parameters are   x = [Tc7, Qimp, Tb8, mdot, M, R, kncrit]
+	#p0 = emcee.utils.sample_ball([3.0,13.0],[0.5,0.5],nwalkers)
+	#p0 = emcee.utils.sample_ball([3.0,13.0,1.4,10.0],[0.5,0.5,0.1,1.0],nwalkers)
+	p0 = emcee.utils.sample_ball([9.0,1.0,4.0,0.1,1.2,13.0],[0.3,0.1,0.3,0.01,0.01,0.5],nwalkers)
+	#p0 = emcee.utils.sample_ball([7.7,0.5,4.0,0.1],[0.3,0.3,0.3,0.01],nwalkers)
+	#p0 = emcee.utils.sample_ball([7.7,0.5,4.0],[0.3,0.3,0.3],nwalkers)
+
+	sampler=emcee.EnsembleSampler(nwalkers,ndim,lnprob,threads=15)
+
+	print 'Starting run at ', str(datetime.datetime.now())
+	start_time = time.time()
+	sampler.run_mcmc(p0, 1000)
+	print 'time to run = ',time.time() - start_time,'seconds'
+	print("Mean acceptance fraction: {0:.3f}"
+	                .format(numpy.mean(sampler.acceptance_fraction)))
+
+	# throw away the first burn steps as burn-in
+	nburn = 0
+	samples=sampler.chain[:,nburn:,:].reshape((-1,ndim))
+
+	os.makedirs('mcmc/'+dir)
+	outputFile = open('mcmc/'+dir+'/samples.dat','w')
+	numpy.save(outputFile, samples)
+	outputFile.close()
+	outputFile = open('mcmc/'+dir+'/sampler_chain.dat','w')
+	numpy.save(outputFile, sampler.chain)
+	outputFile.close()
+	outputFile = open('mcmc/'+dir+'/sampler_lnprobability.dat','w')
+	numpy.save(outputFile, sampler.lnprobability)
+	outputFile.close()
+
+	fp = open('mcmc/'+dir+'/chi.dat','w')
+	probs=sampler.lnprobability.reshape([-1])
+	for i,prob in enumerate(probs):
+		print >>fp,i,-2.0*prob," ".join(str(x) for x in samples[i,:])
+	fp.close()
+
+	os.system('cp mcee.py mcmc/'+dir+'/')
+
+
 def set_params(x,name):
 	data="""output	0
-mass	1.2
-radius	13.0
+mass	1.6
+radius	11.2
 Bfield 	0
 mdot	0.1
 precalc	1
@@ -44,9 +95,9 @@ Tc	7.7e7
 		'Tc':x[0]*1e7,
 		'Qimp':10.0**x[1],
 		'Tt':x[2]*1e8,
-#		'mdot':x[3],
-#		'mass':x[2],
-#		'radius':x[3],
+		'mdot':x[3],
+		'mass':x[4],
+		'radius':x[5],
 #		'extra_Q':x[0],
 #		'extra_y':x[1]
 	}
@@ -66,74 +117,29 @@ def get_chisq(x):
 	data = subprocess.check_output( ["crustcool",name,'1'])
 	chisq = float(re.search('chisq = ([-+]?[0-9]*\.?[0-9]+)',data).group(1))
 #	print x[0],x[1],x[2],x[3],x[4],x[5],x[6],chisq
-	os.system('rm /tmp/init.dat.'+name)	
+	os.system('rm /tmp/init.dat.'+name)
 	return chisq
 
 
 def lnprob(x):
 	# minimum and maxiumum allowed values
 	# (assume a flat prior within this range)
-	#xmin=numpy.array([0.0,-3.0,0.0,0.0,1.1,8.0])
-	#xmax=numpy.array([100.0,3.0,100.0,3.0,2.5,16.0])
-	xmin=numpy.array([0.0,-3.0,0.0])
-	xmax=numpy.array([100.0,3.0,100.0])
+	xmin=numpy.array([1.0,-3.0,0.1,0.0,1.1,8.0])
+	xmax=numpy.array([100.0,3.0,100.0,3.0,2.5,16.0])
+	#xmin=numpy.array([0.0,-3.0,0.0])
+	#xmax=numpy.array([100.0,3.0,100.0])
 	#xmin=numpy.array([0.0,-3.0,0.0,0.0])
 	#xmax=numpy.array([100.0,3.0,100.0,1.0])
 	if (len((x<xmin).nonzero()[0])>0 or len((x>xmax).nonzero()[0])>0):
 		return -numpy.inf
+	# causal limit  radius has to be R > 4.36km (M/Msun)
+	if (x[5] < 4.36*x[4]):
+		return -numpy.inf
+	#print 'Trying ',x
 	chisq=get_chisq(x)
-	#print x[0],x[1],x[2],x[3],chisq
+	#print x[0],x[1],x[2],x[3],x[4],x[5],chisq
 	return -chisq/2.0
 
 
-nwalkers, ndim = 100, 3
-dir = '9'
-if os.path.exists('mcmc/'+dir):
-	print 'The output directory mcmc/'+dir+' already exists!'
-	exit()
-
-# parameters are   x = [Tc7, Qimp, Tb8, mdot, M, R, kncrit]
-#p0 = emcee.utils.sample_ball([3.0,13.0],[0.5,0.5],nwalkers)
-#p0 = emcee.utils.sample_ball([3.0,13.0,1.4,10.0],[0.5,0.5,0.1,1.0],nwalkers)
-#p0 = emcee.utils.sample_ball([20.0,1.0,1.5,2.0,1.2,13.0],[0.3,0.1,0.3,0.2,0.01,0.5],nwalkers)
-#p0 = emcee.utils.sample_ball([7.7,0.5,4.0,0.1],[0.3,0.3,0.3,0.01],nwalkers)
-p0 = emcee.utils.sample_ball([7.7,0.5,4.0],[0.3,0.3,0.3],nwalkers)
-
-sampler=emcee.EnsembleSampler(nwalkers,ndim,lnprob,threads=15)
-
-print 'Starting run at ', str(datetime.datetime.now())
-start_time = time.time()
-sampler.run_mcmc(p0, 500)
-print 'time to run = ',time.time() - start_time,'seconds'
-print("Mean acceptance fraction: {0:.3f}"
-                .format(numpy.mean(sampler.acceptance_fraction)))
-
-# throw away the first burn steps as burn-in
-nburn = 0
-samples=sampler.chain[:,nburn:,:].reshape((-1,ndim))
-fig = triangle.corner(samples,labels=[r"$T_{c,7}$", r"$Q_{imp}$", r"$T_{b,8}$",
-				r"$\dot M$"],
-				quantiles=[0.16, 0.5, 0.84])
-fig.savefig("mcmc/triangle.pdf")
-#fig = triangle.corner(samples,labels=[r"$T_{c,7}$", r"$Q_{imp}$", r"$T_{b,8}$",
-#						r"$\dot M$", r"$M (M_\odot)$", r"$R (km)$", r"k_{n,crit}"],
-#		quantiles=[0.16, 0.5, 0.84])
-
-os.makedirs('mcmc/'+dir)
-outputFile = open('mcmc/'+dir+'/samples.dat','w')
-numpy.save(outputFile, samples)
-outputFile.close()
-outputFile = open('mcmc/'+dir+'/sampler_chain.dat','w')
-numpy.save(outputFile, sampler.chain)
-outputFile.close()
-outputFile = open('mcmc/'+dir+'/sampler_lnprobability.dat','w')
-numpy.save(outputFile, sampler.lnprobability)
-outputFile.close()
-
-fp = open('mcmc/'+dir+'/chi.dat','w')
-probs=sampler.lnprobability.reshape([-1])
-for i,prob in enumerate(probs):
-	print >>fp,i,-2.0*prob," ".join(str(x) for x in samples[i,:])
-fp.close()
-
-os.system('cp mcee.py mcmc/'+dir+'/')
+if __name__ == '__main__':
+    main()
--- mcplot.py
+++ mcplot.py
@@ -5,33 +5,124 @@ import matplotlib.pyplot as plt
 import triangle
 import sys
 
-dir = ''
-if len(sys.argv) > 1:
-	dir += sys.argv[1]
-	dir += '/'
+def main():
 
-fp = open('mcmc/'+dir+'samples.dat','r')
-samples = numpy.load(fp)
-fp.close()
+	# get the directory name from the command line args
+	dir = ''
+	if len(sys.argv) > 1:
+		dir += sys.argv[1]
+		dir += '/'
 
-nparams = samples.shape[1]
-n = samples.shape[0]
-samples = samples[1.0*n/4:,:]
+	samples=read_samples(dir)
+	plot_triangle(samples,dir)
 
-#fig = triangle.corner(samples,labels=[r"$T_{c,7}$", r"$Q_{imp}$", r"$T_{b,8}$",
-#						r"$\dot M$", r"$M (M_\odot)$", r"$R (km)$", r"$k_{n,crit}$"],
-#		quantiles=[0.16, 0.5, 0.84], plot_datapoints=True,bins=24,plot_ellipse=False)
-#fig = triangle.corner(samples,labels=[r"$Q\ (MeV)$", r"$y\ (g\ cm^{-2})$"],
-#		quantiles=[0.16, 0.5, 0.84], plot_datapoints=True,bins=24,plot_ellipse=False)
+	if 0:
+		# 1D histograms of gravity
+		samples=read_samples('13/')
+		plot_hist1d(samples,'r','T=54 eV')
+		plt.xlim([0.5,3.0])
+		plt.ylim([0,0.13])
+		samples=read_samples('14/')
+		plot_hist1d(samples,'b','T=63.1 eV')
+		plt.xlabel(r'$g_{14}$')
+		samples=read_samples('12/')
+		plot_hist1d(samples,'k','current data')
+		plt.legend()
+		plt.savefig('g14_new.pdf')
 
-if nparams == 4:
-	fig = triangle.corner(samples,labels=[r"$T_{c,7}$", r"$Q_{imp}$", r"$T_{b,8}$",
-					r"$\dot M$"], extents=[(4,11),(-3,2),(2,6),(0,0.5)],
-		quantiles=[0.16, 0.5, 0.84], plot_datapoints=False,bins=50,plot_ellipse=False)
+	if 0:
+		# 2D contours for Tc and gravity
+		plt.subplot(2,2,1)
+		samples=read_samples('12/')
+		plot_hist(samples,'r')
 
-if nparams == 3:
-	fig = triangle.corner(samples,labels=[r"$T_{c,7}$", r"$Q_{imp}$", r"$T_{b,8}$"],
-		extents=[(4,11),(-3,2),(2,6)],
+		plt.subplot(2,2,2)
+		samples=read_samples('13/')
+		plot_hist(samples,'r')
+
+		plt.subplot(2,2,4)
+		samples=read_samples('14/')
+		plot_hist(samples,'r')
+
+		plt.savefig('Tc_g14_new.pdf')
+
+
+def read_samples(dir):
+	
+	# read in the samples
+	fp = open('mcmc/'+dir+'samples.dat','r')
+	samples = numpy.load(fp)
+	fp.close()
+	# find out how many parameters
+	nparams = samples.shape[1]
+	# number of points
+	n = samples.shape[0]
+	# remove burn in points
+	samples = samples[1.0*n/4:,:]
+	
+	return samples
+
+
+def plot_hist1d(samples,col_string,label_string):
+
+	n = samples.shape[0]
+	M = samples[:,4]
+	R = samples[:,5]
+	#M = numpy.random.rand(n)*(2.5-1.1) + 1.1
+	#R = numpy.random.rand(n)*(16.0-8.0) + 8.0
+	g = (6.67e-8*2e33/(1e14*1e10)) * M / R**2
+	zz = 1.0/(1.0 - (2.0*1e14*g*1e5*R/9e20))**0.5
+	g*=zz
+	Q = samples[:,1]
+	mdot = samples[:,3]
+	Tc = samples[:,0]
+
+	n1, bins, patches = plt.hist(g, 50, weights=[1.0/n]*n,normed=False,alpha=1.0,color=col_string,histtype='step',label=label_string)
+
+def plot_hist(samples,col_string):
+	
+	M = samples[:,4]
+	R = samples[:,5]
+	g = (6.67e-8*2e33/(1e14*1e10)) * M / R**2
+	zz = 1.0/(1.0 - (2.0*1e14*g*1e5*R/9e20))**0.5
+	g*=zz
+	Q = samples[:,1]
+	mdot = samples[:,3]
+	Tc = samples[:,0]
+	
+	#n, bins, patches = plt.hist(g, 25, normed=1,alpha=0.1,color=col_string)
+	
+	
+	triangle.hist2d(Tc,g,plot_datapoints=False,bins=25,extent=[(2,12),(0.5,3)])
+	plt.xlabel(r'$T_c (10^7 K)$')
+	plt.ylabel(r'$g_{14}$')
+	#triangle.hist2d(M/R,Q,plot_datapoints=False,bins=25,extent=[(0,0.33),(-3,2)])
+	#triangle.hist2d(g,Q,plot_datapoints=False,bins=30,extent=[(0.5,2.5),(-3,2)],
+	#			color=col_string,plot_contours=False,plot_ellipse=True)
+	
+
+
+def plot_triangle(samples,dir):
+
+	nparams = samples.shape[1]
+
+	if nparams == 4:
+		fig = triangle.corner(samples,labels=[r"$T_{c,7}$", r"$Q_{imp}$", r"$T_{b,8}$",
+						r"$\dot M$"], extents=[(4,11),(-3,2),(2,6),(0,0.5)],
+			quantiles=[0.16, 0.5, 0.84], plot_datapoints=False,bins=50,plot_ellipse=False)
+
+	if nparams == 3:
+		fig = triangle.corner(samples,labels=[r"$T_{c,7}$", r"$Q_{imp}$", r"$T_{b,8}$"],
+			extents=[(4,11),(-3,2),(2,6)],
+					quantiles=[0.16, 0.5, 0.84], plot_datapoints=False,bins=50,plot_ellipse=False)
+
+	if nparams == 6:
+		fig = triangle.corner(samples,labels=[r"$T_{c,7}$", r"$Q_{imp}$", r"$T_{b,8}$",
+				r"$\dot M$", r"$M (M_\odot)$", r"$R (km)$"],extents=[(3,11),(-3,2),(2,6),(0,0.5),(1.2,2.4),(8,16)],
 				quantiles=[0.16, 0.5, 0.84], plot_datapoints=False,bins=50,plot_ellipse=False)
 
-fig.savefig('mcmc/'+dir+'mcplot.png')
+	fig.savefig('mcmc/'+dir+'mcplot.png')
+
+
+if __name__ == '__main__':
+    main()
